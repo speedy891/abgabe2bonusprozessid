56fead54a6d05814e9c3825a5ce334b86bea60ea
speedy891 <speedy891@users.noreply.github.com>
2016-12-09 16:27:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   
-   //var pid2 = process.pid;
+
    var process = require('process'); 
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end('Process ID: '+process.pid); 
